9027367f966e1c4867887f774673f2c6757d7592
Tim-60 <999666333111000@rambler.ru>
2025-04-16 16:03:21 +0300

    Update 3.py
    Исправление небольшой неточности в коде.

--- 3.py
+++ 3.py
@@ -1,15 +1,14 @@
 def file(text: str, name: str):
-    with open("C:\\Users\\tim\\Desktop\\" + name, "a", encoding="ANSI") as file:
+    with open(name, "a", encoding="ANSI") as file:
         file.write('\n' + text)
         file.close()
-    with open("C:\\Users\\tim\\Desktop\\" + name, "r", encoding="ANSI") as file:
+    with open(name, "r", encoding="ANSI") as file:
         lines = file.readlines()
         even_lines = list(enumerate(lines, start = 0))
         for i in range(0, len(even_lines), 2):
             print(even_lines[i][1].rstrip('\n'))
         file.close()
-
-file_name = input('Введите имя файла, который будет открыт: ')
+path = "C:\\Users\\tim\\Desktop\\" #Необязательная строчка в коде. Используется для удобства доступа и ввода
+file_name = input('Введите имя файла, который будет открыть: ')
 file_text = input('Введите текст, который будет дописан в файл: ')
-file(file_text, file_name + '.txt')
-
+file(file_text, path + file_name + '.txt')
